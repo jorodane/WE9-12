9cb827b493ffbe882ce3da0c53fbee21fe7a3c05
2zumi <zxvc7777@gmail.com>
2022-04-24 11:10:58 +0900

    Update Message.h

--- WE9-12/Message.h
+++ WE9-12/Message.h
@@ -78,7 +78,10 @@ void BroadCastMessage(char* message, int length, int sendFD = -1, bool sendSelf 
 //메시지를 구분하는 용도                   길이 받을 int 주세요!
 MessageInfo ProcessMessage(char input[4])
 {
-	byteConvertor.character = input;
+	for (int i = 0; i < 4; i++)
+	{
+		byteConvertor.character[i] = input[i];
+	};
 	//메시지타입		길이
 	//[][]			[][]
 
