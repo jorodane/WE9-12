4d66c103a884f9cad1a48a92d56849786772321b
2zumi <zxvc7777@gmail.com>
2022-05-01 11:20:35 +0900

    Update Message.h

--- WE9-12/Message.h
+++ WE9-12/Message.h
@@ -122,7 +122,7 @@ int TranslateMessage(int fromFD, char* message, int messageLength, MessageInfo* 
 			sendResult[8] = 0;
 			cout << "Login Failed" << endl;
 		};
-		SendMessage(sendResult, 9, fromFD);
+		BroadCastMessage(sendResult, 9, fromFD);
 		break;
 	}
 	case MessageType::LogOut:
