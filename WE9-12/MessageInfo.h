28030f80b9012b4701643365109b3d6b6c34c670
2zumi <zxvc7777@gmail.com>
2022-05-08 11:30:40 +0900

    Update MessageInfo.h

--- WE9-12/MessageInfo.h
+++ WE9-12/MessageInfo.h
@@ -77,11 +77,13 @@ public:
 class MessageInfo_Input : public MessageInfo
 {
 public:
-	InputType type;
+	InputType currentType;
 
 	MessageInfo_Input(InputType wantType, int targetUser)
 	{
-		type = wantType;
+		type = MessageType::Input;
+
+		currentType = wantType;
 		userIndex = targetUser;
 	}
 };
