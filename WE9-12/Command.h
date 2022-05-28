6ad88b05dbbcf7dfd102401cbd240e8187435715
2zumi <zxvc7777@gmail.com>
2022-05-28 10:47:41 +0900

    Add Command Thread

---
+++ WE9-12/Command.h
@@ -0,0 +1,27 @@
+#pragma once
+void* CommandThread(void* data)
+{
+	string input;
+	while (isRunning)
+	{
+		cin >> input;
+		for (int i = 0; i < input.size(); i++)
+		{
+			//소문자를 찾아서
+			if (input[i] >= 'a' || input[i] <= 'z')
+			{
+				//소문자를 대문자로 만들어줍시다!
+				input[i] -= 'A' - 'a';
+			};
+		};
+
+		switch (input)
+		{
+		case "QUIT":
+		case "END":
+		case "EXIT":
+			isRunning = false;
+			break;
+		};
+	};
+}
--- WE9-12/Server.cpp
+++ WE9-12/Server.cpp
@@ -71,8 +71,12 @@ pthread_t commandThread;
 //현재 유저 수
 unsigned int currentUserNumber = 0;
 
+//서버가 지금 돌고 있는지 여부
+bool isRunning = false;
+
 void EndFD(struct pollfd* targetFD);
 int StartServer(int currentFD);
+
 
 //왜 #include가 여기에 있나요?
 //헤더는 복사 붙여넣기라서 여기에 있어야 위에 있는 변수들을 사용할 수 있어서 여기에다 뒀어요!
@@ -81,12 +85,13 @@ int StartServer(int currentFD);
 #include "User.h"
 #include "MessageInfo.h"
 #include "Message.h"
+#include "Command.h"
 
 //유저들의 메시지를 보내는 스레드입니다!
 void* SendThread(void* data)
 {
 	int checkNumber;
-	while (true)
+	while (isRunning)
 	{
 		checkNumber = 0;
 		//유저 전체 돌아주기!
@@ -125,8 +130,8 @@ int main()
 	//여기서 FD는 준비가 되었고! 서버를 돌려봅시다!
 	//리슨 소켓의 정보를 전달해주면서 서버를 시작할 거에요!
 	StartServer(ListenFD.fd);
-
-	while (true)
+	//서버가 도는지 확인해볼 거에요!
+	while (isRunning)
 	{
 		//poll에 대해서 말씀을 드릴 때! 누군가 저한테 메시지를 전달했을 때 실행!
 		//0번까지도 폴에 넣어서 리슨 소켓에 대답이 있을 때에도 들어갈 수 있게 위에서 설정해줬어요!
@@ -299,6 +304,11 @@ int StartServer(int currentFD)
 	if (pthread_create(&sendThread, NULL, SendThread, NULL) != 0)
 	{
 		cout << "Cannot Create Send Thread" << endl;
+		return -1;
+	};
+	if (pthread_create(&sendThread, NULL, CommandThread, NULL) != 0)
+	{
+		cout << "Cannot Create Command Thread" << endl;
 		return -1;
 	};
 
